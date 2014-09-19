df2bc2fb430ee4fdadc66e6213b5ae9133028323
Lance Wicks <lw@judocoach.com>
2014-09-19 21:16:47 +0100

    Perltidied main PM - next step try Dancer2

--- Judo-Clubmanager/lib/Judo/Clubmanager.pm
+++ Judo-Clubmanager/lib/Judo/Clubmanager.pm
@@ -5,62 +5,49 @@ use Dancer::Plugin::Auth::Extensible;
 
 use Judo::Club qw( add_club get_clubs get_club );
 
-
-
 our $VERSION = '0.1';
 
 get '/' => sub {
     template 'index';
 };
 
-
-get '/admin'  => require_role Admin => sub {
+get '/admin' => require_role Admin => sub {
     template 'admin/home';
 };
-
 
 # Admin Clubs
 get '/admin/clubs' => require_role Admin => sub {
     my @clubs = get_clubs();
-	template 'admin/clubs/home',
-	{
-		clubs => @clubs,
-	};
+    template 'admin/clubs/home', { clubs => @clubs, };
 };
 
 get '/admin/clubs/add' => require_role Admin => sub {
-	template 'admin/clubs/add';
+    template 'admin/clubs/add';
 };
 
 post '/admin/clubs/add' => require_role Admin => sub {
     my %args = params();
     my %club = add_club(%args);
-    template 'admin/clubs/add',
-    { club => \%club };
+    template 'admin/clubs/add', { club => \%club };
 };
-
 
 get '/admin/clubs/:club' => require_role Admin => sub {
 
-    my $club = get_club(param('club'));
+    my $club = get_club( param('club') );
 
-	template 'admin/clubs/view',
-    {
-        club => $club,
-    };
+    template 'admin/clubs/view', { club => $club, };
 };
 
-
-get '/admin/initdb'  => require_role Admin => sub {
-	template 'admin/initdb';
+get '/admin/initdb' => require_role Admin => sub {
+    template 'admin/initdb';
 };
 
-post '/admin/initdb'  => require_role Admin => sub {
-	Judo::Database::init_db();
-	template 'admin/initdb',
-	{ msg => 'Database Initialised! <br /><a href="/admin">Return to admin page</a>'};
+post '/admin/initdb' => require_role Admin => sub {
+    Judo::Database::init_db();
+    template 'admin/initdb',
+        { msg =>
+            'Database Initialised! <br /><a href="/admin">Return to admin page</a>'
+        };
 };
-
-
 
 true;
